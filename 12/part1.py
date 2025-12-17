37104ffc1d97e97b783457fe4be601d3aae8b352
pawanacharya73 <pawanacharya832@gmail.com>
2025-12-17 22:00:26 +0545

    feat: still can't get it

---
+++ 12/part1.py
@@ -0,0 +1,39 @@
+# I did not come up with this solution; Jonathan Paulson did.
+# I wrote this to understand the problem better (and failed).
+# I wil try again tomorrow to come up with a solution myself.
+
+with open("input.txt", "r") as f:
+    data = f.read()
+
+parts = data.split("\n\n")
+presents = parts[:-1]
+
+SIZES = {}
+for present in presents:
+    lines = present.splitlines()
+    name = int(lines[0][:-1])
+    G = [list(row) for row in lines[1:]]
+    size = 0
+    for row in G:
+        for c in row:
+            if c == "#":
+                size += 1
+    SIZES[name] = size
+
+ans = 0
+regions = parts[-1]
+for region in regions.splitlines():
+    sz, ns = region.split(": ")
+    R, C = [int(x) for x in sz.split("x")]
+    ns = [int(x) for x in ns.split()]
+    total_present_size = sum(n * SIZES[i] for i, n in enumerate(ns))
+    total_grid_size = R * C
+    print(f"{total_grid_size=} {total_present_size=}")
+    if total_present_size * 1.3 < total_grid_size:
+        ans += 1
+    elif total_present_size > total_grid_size:
+        pass
+    else:
+        print(f"HARD {total_grid_size=} {total_present_size=}")
+
+print(ans)
